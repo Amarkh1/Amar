eff16fd694725de74e2e7c104f6791960948b10a
Amarkh1 <amarkhalid530@gmail.com>
2025-06-10 09:29:43 +0200

    Update and rename ttp7.py to keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "PAST_SIMPLE_WORKSHEET['exercise1']['questions'][next_q]}"
+                    )
+                    return EXERCISE1
+                else:
+                    scoreBot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_ = context.user_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
